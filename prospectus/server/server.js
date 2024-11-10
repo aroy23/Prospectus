1e437d40d7339e9e7d87c9bc0a962498c7864f2b
Angela Quan <angelaquan04@g.ucla.edu>
2024-11-10 01:27:04 -0800

    basic firebase authentication/accounts set up

--- prospectus/server/server.js
+++ prospectus/server/server.js
@@ -7,8 +7,49 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+const FB_admin = require('firebase-admin'); 
+const FB_serviceAccount = require('./prospectus-35l-admin.json')
+
+FB_admin.initializeApp({
+    credential: FB_admin.credential.cert(FB_serviceAccount),
+});
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(403).send("unauthorized: no token");
+    }
+
+    const loginToken = authHeader.split("Bearer ")[1];
+    if (!loginToken)
+    {
+        return res.status(403).send("unauth: missing loginToken");
+    }
+
+
+    FB_admin.auth().verifyIdToken(loginToken)
+        .then((decodedToken) => { 
+            req.user = decodedToken;
+            next(); 
+        })
+        .catch((error) => {
+            console.error("error with token verification", error); 
+            res.status(401).send("invalid token"); 
+        }
+        ); 
+}; 
+
 app.get("/api", (req, res) => {
     res.json({"fruits": ["apple", "orange", "banana"] })
+});
+
+
+app.get("/protected-api", verifyToken, (req, res) => {
+    res.json({
+        message: "in protected API",
+        user: req.user, 
+    });
 });
 
 app.listen(8080, () => {
